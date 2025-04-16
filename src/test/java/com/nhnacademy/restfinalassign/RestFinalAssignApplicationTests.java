015877f378ba456ceab48abdfe0bfa391ac7a543
doyun <90900630+Doyun-coding@users.noreply.github.com>
2025-04-16 22:42:05 +0900

    first commit

---
+++ src/test/java/com/nhnacademy/restfinalassign/RestFinalAssignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.restfinalassign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestFinalAssignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
